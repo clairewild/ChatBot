df4284eb540ca916437bc24888e64def4df6ce12
Claire Wild <claire.wild15@gmail.com>
2017-06-28 12:31:21 -0700

    Fix quotation marks to make consistent

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ import random
 
 app = Flask(__name__)
 
-token = "EAAMKk8tUJe4BAKheQjTq9Cs1vdXq0LNZC19cR2ZBnt0vZBnW1dsbu43yI5i2bUhZBmSuXV6P6gb1LWLurbFZBopPKZAStZCvx1HSr0KnKgfz1mT79sLiZClfTZBZBvoR1AKYRBHmgBPArH99xHSTsq2JZCrLYj4zUMDxjaNZAqif7koEYgZDZD"
+token = 'EAAMKk8tUJe4BAKheQjTq9Cs1vdXq0LNZC19cR2ZBnt0vZBnW1dsbu43yI5i2bUhZBmSuXV6P6gb1LWLurbFZBopPKZAStZCvx1HSr0KnKgfz1mT79sLiZClfTZBZBvoR1AKYRBHmgBPArH99xHSTsq2JZCrLYj4zUMDxjaNZAqif7koEYgZDZD'
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -15,17 +15,15 @@ def webhook():
       data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
-      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
+      payload = {'recipient': {'id': sender}, 'message': {'text': 'Hello World'}}
+      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
     except Exception as e:
-      print(traceback.format_exception()) # something went wrong
+      print(traceback.format_exception())
 
-  elif request.method == 'GET': # For the initial verification
+  elif request.method == 'GET': # For initial verification
     if request.args.get('hub.verify_token') == 'Hello World':
       return request.args.get('hub.challenge')
-    return "Wrong Verify Token"
-    
-  return "Hello World"
+    return 'Wrong Verify Token'
 
 if __name__ == '__main__':
   app.run(debug=True)
