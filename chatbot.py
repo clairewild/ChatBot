53ecd8abee20dfa2477af2a8a3301ffe33355e4e
Claire Wild <claire.wild15@gmail.com>
2017-07-07 14:25:14 -0700

    Add keys to gitignore and integrate wit.ai

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,26 @@
 from flask import Flask, request
+from wit import Wit
 import requests
 import json
 import traceback
 import random
 
+import keys
+
 app = Flask(__name__)
 
-token = 'EAAMKk8tUJe4BAKheQjTq9Cs1vdXq0LNZC19cR2ZBnt0vZBnW1dsbu43yI5i2bUhZBmSuXV6P6gb1LWLurbFZBopPKZAStZCvx1HSr0KnKgfz1mT79sLiZClfTZBZBvoR1AKYRBHmgBPArH99xHSTsq2JZCrLYj4zUMDxjaNZAqif7koEYgZDZD'
+witClient = Wit(access_token=keys.witToken)
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
   if request.method == 'POST':
     try:
       data = json.loads(request.data)
-      text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'status': '200 OK', 'recipient': {'id': sender}, 'message': {'text': 'Hello World'}}
-      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+      text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
+      response = witClient.message(text) # Outgoing Message Text
+      payload = {'status': '200 OK', 'recipient': {'id': sender}, 'message': {'text': response}}
+      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + keys.fbToken, json=payload)
     except Exception as e:
       print(traceback.format_exception())
 
